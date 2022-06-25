8253da22c8244bf4ecc1c1df28295db1c91937f0
Débora Beda <deborabeda@live.com>
2022-06-26 01:32:40 +0200

    Follow multiples paths on character movement

--- Magalu_GameJam/Assets/Scripts/Core/CharacterMovement.cs
+++ Magalu_GameJam/Assets/Scripts/Core/CharacterMovement.cs
@@ -9,61 +9,121 @@ namespace Core
     {
         [SerializeField]
         private Transform[] routes;
-        private int routeToGo;
 
         [SerializeField]
+        private int routeToGo = 0;
+        [SerializeField]
+        private int previousRoute = 0;
+        [SerializeField]
+        private bool moveFoward = true;
+        [SerializeField]
+        private bool coroutineAllowed = false;
+
         private float speedModifier;
         private float tParam;
         private Vector2 characterPosition;
-        private bool coroutineAllowed;
 
         private void Start()
         {
             routeToGo = 0;
             tParam = 0f;
-            speedModifier = 0.2f;
+            speedModifier = 0.5f;
             coroutineAllowed = true;
         }
 
-        private void Update() {
-            if(coroutineAllowed)
+        private void Update()
+        {
+            if (coroutineAllowed)
             {
-                StartCoroutine(Move(routeToGo));
+                StartCoroutine(Move());
             }
         }
 
-        private IEnumerator Move(int routeNumber) {
+        private IEnumerator Move()
+        {
+            bool previousMV = moveFoward;
+            routeToGo = Random.Range(0, routes.Length);
+
+            if (routeToGo > previousRoute)
+            {
+                moveFoward = true;
+            }
+            else if (routeToGo == previousRoute)
+            {
+                moveFoward = !moveFoward;
+            }
+            else
+            {
+                moveFoward = false;
+            }
+
+            List<Transform> selectedRoutes = new List<Transform>();
+            if (moveFoward)
+            {
+                for (int i = previousRoute; i <= routeToGo; i++)
+                {
+                    if (previousMV && i == previousRoute)
+                    {
+                        continue;
+                    }
+                    selectedRoutes.Add(routes[i]);
+                }
+            }
+            else
+            {
+                for (int i = previousRoute; i >= routeToGo; i--)
+                {
+                    if (!previousMV && i == previousRoute)
+                    {
+                        continue;
+                    }
+                    selectedRoutes.Add(routes[i]);
+                }
+            }
+
+            StartCoroutine(MoveThroughBezier(selectedRoutes, moveFoward));
+
+
+            yield return new WaitForSecondsRealtime(10.0f);
+            previousRoute = routeToGo;
+            coroutineAllowed = true;
+        }
+
+        private IEnumerator MoveThroughBezier(List<Transform> routes, bool moveFoward)
+        {
             coroutineAllowed = false;
 
-            Vector2 p0 = routes[routeNumber].GetChild(0).position;
-            Vector2 p1 = routes[routeNumber].GetChild(1).position;
-            Vector2 p2 = routes[routeNumber].GetChild(2).position;
-            Vector2 p3 = routes[routeNumber].GetChild(3).position;
-
-            while(tParam < 1)
+            foreach (Transform r in routes)
             {
-                tParam += Time.deltaTime * speedModifier;
+                Vector2 p0 = r.GetChild(3).position;
+                Vector2 p1 = r.GetChild(2).position;
+                Vector2 p2 = r.GetChild(1).position;
+                Vector2 p3 = r.GetChild(0).position;
 
-                characterPosition = Mathf.Pow(1 - tParam, 3) * p0 +
-                3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 +
-                3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 +
-                Mathf.Pow(tParam, 3) * p3;
+                if (moveFoward)
+                {
+                    p0 = r.GetChild(0).position;
+                    p1 = r.GetChild(1).position;
+                    p2 = r.GetChild(2).position;
+                    p3 = r.GetChild(3).position;
+                }
 
-                transform.position = characterPosition;
-                yield return new WaitForEndOfFrame();
+                while (tParam < 1)
+                {
+                    tParam += Time.deltaTime * speedModifier;
+
+                    characterPosition = Mathf.Pow(1 - tParam, 3) * p0 +
+                    3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 +
+                    3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 +
+                    Mathf.Pow(tParam, 3) * p3;
+
+                    transform.position = characterPosition;
+                    yield return new WaitForEndOfFrame();
+                }
+
+                tParam = 0f;
             }
 
-            tParam = 0f;
-
-            routeToGo += 1;
-
-            if(routeToGo > routes.Length - 1)
-            {
-                routeToGo = 0;
-            }
-
-            coroutineAllowed = true;
         }
     }
 }
-
